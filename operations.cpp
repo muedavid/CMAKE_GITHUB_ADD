4b3cdd733ef9aa2ff7a6e60c784db5add8acb23a
muedavid <muedavid@ethz.ch>
2022-10-06 09:50:45 +0200

    initialized github library

---
+++ operations.cpp
@@ -0,0 +1,26 @@
+//
+// Created by david on 06.10.22.
+//
+
+#include "operations.h"
+
+namespace math_git{
+int operations::sum(const int &a, const int &b) {
+  return a + b;
+}
+
+int operations::mult(const int &a, const int &b) {
+  return a * b;
+}
+
+int operations::div(const int &a, const int &b) {
+  if (b == 0) {
+    throw std::overflow_error("Divide by zero exception");
+  }
+  return a / b;
+}
+
+int operations::sub(const int &a, const int &b) {
+  return a - b;
+}
+}
---
+++ operations.h
@@ -0,0 +1,19 @@
+//
+// Created by david on 06.10.22.
+//
+
+#ifndef CMAKE_GITHUB_ADD_OPERATIONS_H
+#define CMAKE_GITHUB_ADD_OPERATIONS_H
+
+namespace math_git {
+class operations {
+public:
+  int sum(const int &a, const int &b);
+  int mult(const int &a, const int &b);
+  int div(const int &a, const int &b);
+  int sub(const int &a, const int &b);
+};
+};
+}
+
+#endif //CMAKE_GITHUB_ADD_OPERATIONS_H
